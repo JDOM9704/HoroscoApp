242333baa73e9cb30a19d81c297cbb37a8734943
Juan David <juan97ortega52@gmail.com>
2025-08-29 16:11:54 -0500

    Se agrega menu de navegacion y se configura la navegacion con los diferentes fragments

--- app/src/main/java/com/android/example/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/android/example/horoscapp/ui/home/MainActivity.kt
@@ -3,18 +3,33 @@ package com.android.example.horoscapp.ui.home
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.android.example.horoscapp.R
 import com.android.example.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+initUI()
+    }
 
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost : NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
